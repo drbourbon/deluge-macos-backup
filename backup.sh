127ceecb6c5ab1f48245bf2d40311e68a537ca5a
Fabio Barbon <fabio.barbon@gmail.com>
2023-02-20 15:23:42 +0100

    added compatibility to deluge firmware v4.x

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#BACKUP_PATH=~/Documents/Deluge-Backup
 BACKUP_SCRIPT=./rsync_tmbackup.sh 
 INCLUDE_FILE=/tmp/deluge_include.txt
 SD_PATH=../../..
@@ -10,14 +9,15 @@ BACKUP_PATH=~/Documents/Deluge-Backup/${SD_NAME// /_}
 #echo $BACKUP_PATH
 #exit
 
-#echo -e "+ SONGS/*.XML\n+ KITS/*.XML\n+ SYNTHS/*.XML\n+ SAMPLES/RESAMPLE/*.WAV" > $INCLUDE_FILE
-#cat $SD_PATH/SONGS/*.XML $SD_PATH/KITS/*.XML $SD_PATH/SYNTHS/*.XML | sed -ne '/fileName/{s/.*<fileName>\(.*\)<\/fileName>.*/+ \1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
-#echo "- *" >> $INCLUDE_FILE
-
+# Adds XML files themselves
 find $SD_PATH -name "*.XML" > $INCLUDE_FILE
-cat $SD_PATH/SONGS/*.XML $SD_PATH/KITS/*.XML $SD_PATH/SYNTHS/*.XML | sed -ne '/fileName/{s/.*<fileName>\(.*\)<\/fileName>.*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
-cat $SD_PATH/SONGS/*.XML $SD_PATH/KITS/*.XML $SD_PATH/SYNTHS/*.XML | sed -ne '/filePath/{s/.*filePath="\(.*\)".*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
-cat $SD_PATH/SONGS/*.XML $SD_PATH/KITS/*.XML $SD_PATH/SYNTHS/*.XML | sed -ne '/fileName/{s/.*fileName="\(.*\)".*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
+
+# Adds samples referenced in some XML file
+find $SD_PATH -name "*.XML" -exec cat {} + | sed -ne '/fileName/{s/.*<fileName>\(.*\)<\/fileName>.*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
+find $SD_PATH -name "*.XML" -exec cat {} + | sed -ne '/filePath/{s/.*filePath="\(.*\)".*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
+find $SD_PATH -name "*.XML" -exec cat {} + | sed -ne '/fileName/{s/.*fileName="\(.*\)".*/\1/p;}' | sort -u | tail -n +2 >> $INCLUDE_FILE
+
+# Adds all samples from downmix fixed folder
 find $SD_PATH/SAMPLES/RESAMPLE -name "*.WAV" >> $INCLUDE_FILE
 
 #echo "PROGRESS:0"
@@ -25,8 +25,6 @@ mkdir -p "$BACKUP_PATH"
 touch "$BACKUP_PATH/backup.marker"
 
 #echo "PROGRESS:10"
-#$BACKUP_SCRIPT --rsync-append-flags "-q" "$SD_PATH" "$BACKUP_PATH"
-#$BACKUP_SCRIPT "$SD_PATH" "$BACKUP_PATH"
 $BACKUP_SCRIPT --rsync-append-flags "--files-from=$INCLUDE_FILE" "$SD_PATH" "$BACKUP_PATH"
 
 #echo "PROGRESS:90"
